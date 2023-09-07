7fe04bb86955f3fa97a3e0a6dfb30a6e8212c214
Vitalii <mkukoba11@ukr.net>
2023-09-07 17:01:41 +0300

    added CustomGlobalException, and added validation to CreateBookRequestDto

--- src/main/java/com/onlinebookshop/controller/BookController.java
+++ src/main/java/com/onlinebookshop/controller/BookController.java
@@ -4,6 +4,7 @@ import com.onlinebookshop.dto.book.BookDto;
 import com.onlinebookshop.dto.book.BookSearchParameters;
 import com.onlinebookshop.dto.book.CreateBookRequestDto;
 import com.onlinebookshop.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -40,12 +41,12 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public BookDto update(@PathVariable Long id, @RequestBody CreateBookRequestDto params) {
+    public BookDto update(@PathVariable Long id, @RequestBody @Valid CreateBookRequestDto params) {
         return bookService.update(id, params);
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
--- src/main/java/com/onlinebookshop/dto/book/CreateBookRequestDto.java
+++ src/main/java/com/onlinebookshop/dto/book/CreateBookRequestDto.java
@@ -1,13 +1,25 @@
 package com.onlinebookshop.dto.book;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Size(min = 1, max = 100)
     private String title;
+    @NotNull
+    @Size(min = 1, max = 100)
     private String author;
+    @NotNull
+    @ISBN
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
