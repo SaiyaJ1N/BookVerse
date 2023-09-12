5ffb8321b1f1afcc9126c20a8153b2f655ae13da
Vitalii <mkukoba11@ukr.net>
2023-09-12 08:46:55 +0300

    Fixed boorRepository imports

--- src/main/java/com/onlinebookshop/repository/book/BookRepository.java
+++ src/main/java/com/onlinebookshop/repository/book/BookRepository.java
@@ -1,7 +1,6 @@
 package com.onlinebookshop.repository.book;
 
 import com.onlinebookshop.model.Book;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
